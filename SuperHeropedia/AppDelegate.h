e2d9e29fcde89ae6d4e4d5dda14c0001345b87cf
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-13 10:50:55 -0500

    Initial Commit

---
+++ SuperHeropedia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuperHeropedia
+//
+//  Created by Eduardo Alvarado Díaz on 10/13/14.
+//  Copyright (c) 2014 Globant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
